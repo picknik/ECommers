7294f03ca35b9301a21b64cbe6eeb526578e56f0
max iv <max.user.w@gmail.com>
2015-07-20 18:37:09 +0300

    FactoryGirl product factory

--- spec/factories/products.rb
+++ spec/factories/products.rb
@@ -1,5 +1,10 @@
 FactoryGirl.define do
   factory :product do
-    
+    sequence(:name){ |n| "Product_#{n}" }
+    units_in_stock { rand(100..1000) }
+    units_on_order { rand(1..5) }
+    discount       { rand(0.1..0.3).round(2) }
+    price          { rand(10..500) }
+    reorder_level  { rand(10..100) }
   end
 end
